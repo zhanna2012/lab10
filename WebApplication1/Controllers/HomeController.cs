04d341d8ed8d2faf2bc9d3681192dc23751b8485
zhanna2012 <rudenko.zhanna.2002@gmail.com>
2022-12-14 22:38:26 +0200

    initial files

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(new FormData());
+    }
+
+    [HttpPost]
+    public IActionResult Index([FromForm] FormData form)
+    {
+        if (form.ConsultationDate < DateTime.Now)
+        {
+            ModelState.AddModelError(nameof(FormData.ConsultationDate), "Consultation should be in future");
+            return View(form);
+        }
+
+        if (form.LanguageConsulstation == "Основи" && form.ConsultationDate.DayOfWeek == DayOfWeek.Monday)
+        {
+            ModelState.AddModelError(nameof(FormData.LanguageConsulstation), "Language consulatation for 'Основи' can't be on Monday");
+            return View(form);
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(form);
+        }
+
+        return RedirectToAction(nameof(FormCreated));
+    }
+
+    public IActionResult FormCreated() => View();
+}
---
+++ WebApplication1/Models/FormData.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class FormData
+{
+    [Required]
+    public string FullName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public DateTime ConsultationDate { get; set; }
+
+    [Required]
+    public string LanguageConsulstation { get; set; }
+
+    public string[] Languages => new[] { "JavaScript", "C#", "Java", "Python", "Основи" };
+}
